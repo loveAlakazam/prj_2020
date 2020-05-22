0c075d5e06cb1373b1fc6a131f1aec204222c21f
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2020-05-23 01:24:50 +0900

    Create Run_Hard.java

---
+++ kh_academy/homework/hw01/4_제어문/Run_Hard.java
@@ -0,0 +1,9 @@
+package com.kh.practice.chap01;
+
+public class Run_Hard {
+	public static void main(String[] args) {
+		ControlPractice_Hard cp = new ControlPractice_Hard();
+    cp.practice11();
+		
+	}
+}
