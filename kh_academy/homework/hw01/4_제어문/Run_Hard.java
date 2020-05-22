b1e9637418eb619fc5b9221da763fb2d8719e7cf
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2020-05-23 01:25:10 +0900

    Update Run_Hard.java

--- kh_academy/homework/hw01/4_제어문/Run_Hard.java
+++ kh_academy/homework/hw01/4_제어문/Run_Hard.java
@@ -3,7 +3,6 @@ package com.kh.practice.chap01;
 public class Run_Hard {
 	public static void main(String[] args) {
 		ControlPractice_Hard cp = new ControlPractice_Hard();
-    cp.practice11();
-		
+    		cp.practice11();
 	}
 }
