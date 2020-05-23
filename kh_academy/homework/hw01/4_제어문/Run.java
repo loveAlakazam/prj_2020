9612206369664e0f5affe2396435a56de04ff32e
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2020-05-23 16:10:17 +0900

    kh-과제-제어문

---
+++ kh_academy/homework/hw01/4_제어문/Run.java
@@ -0,0 +1,18 @@
+package com.kh.practice.chap01;
+
+public class Run {
+	public static void main(String[] args) {
+		ControlPractice cp = new ControlPractice();
+//		cp.practice1(); //실습문제 1
+//		cp.practice2(); //실습문제 2
+//		cp.practice3(); //실습문제3
+//		cp.practice4(); //실습문제4
+//		cp.practice5(); //실습문제5
+//		cp.practice6(); //실습문제6
+//		cp.practice7(); //실습문제7
+//		cp.practice8(); //실습문제8
+//		cp.practice9(); //실습문제9
+		cp.practice10();
+//		cp.practice11();//실습문제 11
+	}
+}
